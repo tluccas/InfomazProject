676750120cb158bc931255b7d571d6be03ee62bd
Lucas Alves <138324870+tluccas@users.noreply.github.com>
2025-06-01 16:30:47 -0300

    style: formatação

--- src/main/java/org/example/util/ConexaoFactory.java
+++ src/main/java/org/example/util/ConexaoFactory.java
@@ -3,9 +3,9 @@ package org.example.util;
 import java.sql.*;
 
 public class ConexaoFactory{ //OBS: usei MySQL os arquivos estão na pasta infomazDB
-        private static final String URL = "jdbc:mysql://localhost:3306/infomaz?user=root";
-        private static final String USER = "root";
-        private static final String PASSWORD = "rootlocadora";
+        private static final String URL = "*****";
+        private static final String USER = "****";
+        private static final String PASSWORD = "*****";
 
         public static Connection getConexao() throws SQLException {
             return DriverManager.getConnection(URL, USER, PASSWORD);
